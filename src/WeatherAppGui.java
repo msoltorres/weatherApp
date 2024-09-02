e10971593d98170186ad520a2f4d16f977aff0e1
Milagros Solange Torres <mstorres@gyl.local>
2024-09-02 20:16:17 -0300

    creación del primer componente

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
+/* Se define una nueva clase llamada WeatherAppGui que hereda de JFrame.
+Esto significa que nuestra clase representa una ventana (frame) en la aplicación. */
 public class WeatherAppGui extends JFrame {
+    // constructor para la inicialización de los componentes de la ventana
     public WeatherAppGui() {
-        // seteamos nuestra aplicación y le añadimos un título
+        // añadimos título a la aplicación llamando al constructor de la clase Jframe
         super("Weather App");
 
         // configuración para terminar el programa una vez que se cierre
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // configuramos el tamaño de nuestra gui en pixeles
+        // configuramos el tamaño de nuestra gui
         setSize(450, 650);
 
         // configuración para que nuestra gui esté en el centro de la pantalla
@@ -19,5 +23,20 @@ public class WeatherAppGui extends JFrame {
 
         // configuración que previene que se edite el tamaño de nuestra gui
         setResizable(false);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        // campo de búsqueda
+        JTextField searchTextField = new JTextField();
+
+        // configuramos la locación y tamaño de nuestro componente
+        searchTextField.setBounds(15, 15, 351, 45);
+
+        // cambiamos el estilo y tamaño de la fuente
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+
+        add(searchTextField);
     }
 }
